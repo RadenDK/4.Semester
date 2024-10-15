0b8d7588eb92395a0503c81cea9fce4c54f6812c
Carl <10485521@ucn.dk>
2024-10-15 14:55:13 +0200

    checks for UserRegistrationModel

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/UserRegistrationModel.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/UserRegistrationModel.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FoosballProLeague.Webserver.Models;
 
 public class UserRegistrationModel
 {
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must be at least 8 characters, include at least one upper case letter, one lower case letter, and one numeric digit.")]
     public string Password { get; set; }
 }
