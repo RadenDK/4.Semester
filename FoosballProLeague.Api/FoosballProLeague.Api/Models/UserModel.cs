341493ed4c0cf6e082d3be0bac791d8fb00fedd2
Carl <10485521@ucn.dk>
2024-10-18 14:01:28 +0200

    UserModel update

--- FoosballProLeague.Api/FoosballProLeague.Api/Models/UserModel.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api/Models/UserModel.cs
@@ -7,8 +7,6 @@ public class UserModel
     public string LastName { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
-    public DepartmentModel? Department { get; set; }
-    public CompanyModel? Company { get; set; }
     public int Elo1v1 { get; set; }
     public int Elo2v2 { get; set; }
 }
--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/UserModel.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/UserModel.cs
@@ -1,12 +1,22 @@
+using System.Text.Json.Serialization;
+
 namespace FoosballProLeague.Webserver.Models;
 
 public class UserModel
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+    [JsonPropertyName("firstName")]
     public string FirstName { get; set; }
+    [JsonPropertyName("lastName")]
     public string LastName { get; set; }
+    [JsonPropertyName("email")]
     public string? Email { get; set; }
+    [JsonPropertyName("password")]
+    public string? Password { get; set; }
+    [JsonPropertyName("elo1v1")]
     public int Elo1v1 { get; set; }
+    [JsonPropertyName("elo2v2")]
     public int Elo2v2 { get; set; }
 
 }
