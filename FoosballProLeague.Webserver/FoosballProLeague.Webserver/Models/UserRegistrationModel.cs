df513d7931ebb0c6f6c8793f178a805845837ccb
Carl <10485521@ucn.dk>
2024-10-18 14:08:00 +0200

    Confirm password field

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/UserRegistrationModel.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/UserRegistrationModel.cs
@@ -13,4 +13,8 @@ public class UserRegistrationModel
     [Required(ErrorMessage = "Password is required")]
     [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must be at least 8 characters, include at least one upper case letter, one lower case letter, and one numeric digit.")]
     public string Password { get; set; }
+    
+    [Required(ErrorMessage = "Field is required")]
+    [Compare("Password", ErrorMessage = "Passwords are not equal")]
+    public string ConfirmPassword { get; set; }
 }
