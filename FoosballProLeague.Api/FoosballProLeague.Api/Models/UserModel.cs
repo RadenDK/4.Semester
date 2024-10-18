8b678f176d2282356584386107b7dbd13e1d4b8a
Carl <10485521@ucn.dk>
2024-10-18 12:34:22 +0200

    UserModel in api and webserver changes

--- FoosballProLeague.Api/FoosballProLeague.Api/Models/UserModel.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api/Models/UserModel.cs
@@ -2,13 +2,13 @@ namespace FoosballProLeague.Api.Models;
 
 public class UserModel
 {
-    public int UserId { get; set; }
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public DepartmentModel Department { get; set; }
-    public CompanyModel Company { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public DepartmentModel? Department { get; set; }
+    public CompanyModel? Company { get; set; }
     public int Elo1v1 { get; set; }
     public int Elo2v2 { get; set; }
 }
