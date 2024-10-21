57ecafacf8b11f6ea5de0d08f0e5211a7b6e2054
kimb0kastekniv <10197768@ucn.dk>
2024-10-21 15:20:59 +0200

    Put changed to Post to fix login function

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Service/LoginService.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Service/LoginService.cs
@@ -22,7 +22,7 @@ public class LoginService : ILoginService
    {
        StringContent content = new StringContent(JsonConvert.SerializeObject(new{Email = email, Password = password} ), Encoding.UTF8, "application/json");
        
-       return await _httpClientService.PutAsync("/User/login", content);
+       return await _httpClientService.PostAsync("/User/login", content);
    }
    
    //service call for login out user 
