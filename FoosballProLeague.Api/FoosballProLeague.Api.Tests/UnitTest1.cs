f661f8554c6a360277bce7f9b37b58e603806567
Rasmus <rasmusjensen.dev@gmail.com>
2024-10-21 13:30:31 +0200

    Added a xunit test project for the api

---
+++ FoosballProLeague.Api/FoosballProLeague.Api.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FoosballProLeague.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
