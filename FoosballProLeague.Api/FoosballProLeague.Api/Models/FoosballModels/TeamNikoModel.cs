009aab31252b44201f8fd2bcc38f9874c5b565a7
nikoUCN <10478737@ucn.dk>
2024-10-28 13:42:57 +0100

    Made temporary method to hold a list of users in a team

---
+++ FoosballProLeague.Api/FoosballProLeague.Api/Models/FoosballModels/TeamNikoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FoosballProLeague.Api.Models.FoosballModels
+{
+    public class TeamNikoModel
+    {
+        public int Id { get; set; }
+        public List<UserModel> teamRed { get; set; }
+        public List<UserModel> teamBlue { get; set; }
+    }
+}
