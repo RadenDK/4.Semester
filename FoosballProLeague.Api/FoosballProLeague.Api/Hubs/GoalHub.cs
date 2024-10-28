7d2e65ab50d8186809cfe78d1435c3ffc8186bac
nikoUCN <10478737@ucn.dk>
2024-10-28 13:42:09 +0100

    Made GoalHub to notify clients of goals

---
+++ FoosballProLeague.Api/FoosballProLeague.Api/Hubs/GoalHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using FoosballProLeague.Api.Models.FoosballModels;
+
+namespace FoosballProLeague.Api.Hubs
+{
+    public class GoalHub : Hub
+    {
+
+        public async Task NotifyGoalsScored(TeamNikoModel teamRed, TeamNikoModel teamBlue, int redScore, int blueScore)
+        {
+            await Clients.All.SendAsync("RecieveGoalUpdate", teamRed, teamBlue, redScore, blueScore);
+        }
+    }
+}
