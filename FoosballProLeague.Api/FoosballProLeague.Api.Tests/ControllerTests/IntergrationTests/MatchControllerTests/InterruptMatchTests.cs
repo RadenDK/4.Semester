c40e63d90c7fa0a88ee18ce98f2990b7b46b138a
Rasmus <rasmusjensen.dev@gmail.com>
2024-10-28 09:25:58 +0100

    Added initial test for new endpoints

---
+++ FoosballProLeague.Api/FoosballProLeague.Api.Tests/ControllerTests/IntergrationTests/MatchControllerTests/InterruptMatchTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoosballProLeague.Api.Tests.ControllerTests.IntergrationTests.MatchControllerTests
+{
+    [Collection("Non-Parallel Database Collection")]
+    public class InterruptMatchTests : DatabaseTestBase
+    {
+        [Fact]
+        public void InterruptMatch_WithActiveMatch_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void InterruptMatch_WithNoActiveMatch_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ FoosballProLeague.Api/FoosballProLeague.Api.Tests/ControllerTests/IntergrationTests/MatchControllerTests/StartMatchTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoosballProLeague.Api.Tests.ControllerTests.IntergrationTests.MatchControllerTests
+{
+    [Collection("Non-Parallel Database Collection")]
+    public class StartMatchTests : DatabaseTestBase
+    {
+        [Fact]
+        public void StartMatch_WithValidPendingPlayers_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void StartMatch_WithInvarianceInTeamSize_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void StartMatch_WithNoPendingTeams_ShouldReturnBadRequest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void StartMatch_ShouldCreateTeamIfNoExistingTeam()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void StartMatch_ShouldNotCreateTeamIfExistingTeam()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void StartMatch_WithActiveMatch_ShouldReturnBadRequest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
