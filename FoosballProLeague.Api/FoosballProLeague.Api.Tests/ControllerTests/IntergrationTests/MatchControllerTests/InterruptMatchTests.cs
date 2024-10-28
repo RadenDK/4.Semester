033193b1b9f233e6eec6a20a68ca5ff12f7dcd13
Rasmus <rasmusjensen.dev@gmail.com>
2024-10-28 13:18:09 +0100

    Made tests for the interrupt game

--- FoosballProLeague.Api/FoosballProLeague.Api.Tests/ControllerTests/IntergrationTests/MatchControllerTests/InterruptMatchTests.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api.Tests/ControllerTests/IntergrationTests/MatchControllerTests/InterruptMatchTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using FoosballProLeague.Api.BusinessLogic;
+using FoosballProLeague.Api.Controllers;
+using FoosballProLeague.Api.DatabaseAccess;
+using FoosballProLeague.Api.Models.FoosballModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,20 +18,43 @@ namespace FoosballProLeague.Api.Tests.ControllerTests.IntergrationTests.MatchCon
         public void InterruptMatch_WithActiveMatch_ShouldReturnSuccess()
         {
             // Arrange
+            _dbHelper.InsertData("INSERT INTO users (id) VALUES (1), (2)");
+            _dbHelper.InsertData("INSERT INTO teams (player1_id) VALUES (1), (2)"); // Existing teams for the match
+            _dbHelper.InsertData("INSERT INTO foosball_tables (id) VALUES (1)");
+            _dbHelper.InsertData("INSERT INTO foosball_matches (table_id, red_team_id, blue_team_id, team_red_score, team_blue_score) VALUES (1, 1, 2, 5, 3)"); // Active match
+            _dbHelper.UpdateData("UPDATE foosball_tables SET active_match_id = 1 WHERE id = 1");
+
+            int mockTableId = 1;
+
+            IMatchDatabaseAccessor matchDatabaseAccessor = new MatchDatabaseAccessor(_dbHelper.GetConfiguration());
+            IMatchLogic matchLogic = new MatchLogic(matchDatabaseAccessor);
+            MatchController SUT = new MatchController(matchLogic);
 
             // Act
+            IActionResult result = SUT.InterruptMatch(mockTableId);
 
             // Assert
+            IEnumerable<MatchModel> matches = _dbHelper.ReadData<MatchModel>("SELECT * FROM foosball_matches");
+            IEnumerable<FoosballTableModel> table = _dbHelper.ReadData<FoosballTableModel>("SELECT * FROM foosball_tables");
+
+            MatchModel interruptedMatch = matches.First(); // The match that was interrupted
+            Assert.True(interruptedMatch.EndTime.HasValue); // Ensure the match end time is set
+            Assert.True(table.First().ActiveMatchId == null); // Ensure the table's active match ID is set to null since the match is interrupted
         }
 
         [Fact]
         public void InterruptMatch_WithNoActiveMatch_ShouldReturnSuccess()
         {
             // Arrange
+            IMatchDatabaseAccessor matchDatabaseAccessor = new MatchDatabaseAccessor(_dbHelper.GetConfiguration());
+            IMatchLogic matchLogic = new MatchLogic(matchDatabaseAccessor);
+            MatchController SUT = new MatchController(matchLogic);
 
             // Act
+            IActionResult result = SUT.InterruptMatch(1);
 
             // Assert
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
