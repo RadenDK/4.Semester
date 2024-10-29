ad05fad4330723b56615b23ce98c38bc55d9c8b7
Carl <10485521@ucn.dk>
2024-10-29 15:22:15 +0100

    Methods for UserLogic
    Made GetUserById method
    Made elo calculation for team and individual users

--- FoosballProLeague.Api/FoosballProLeague.Api/BusinessLogic/IUserLogic.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api/BusinessLogic/IUserLogic.cs
@@ -1,4 +1,5 @@
 using FoosballProLeague.Api.Models;
+using FoosballProLeague.Api.Models.FoosballModels;
 
 namespace FoosballProLeague.Api.BusinessLogic;
 
@@ -9,4 +10,8 @@ public interface IUserLogic
     public bool LoginUser(string email, string password);
     public List<UserModel> GetUsers();
     public UserModel GetUser(string email);
+    public UserModel GetUserById(int userId);
+    public bool UpdateUserElo(int userId,int elo, bool is1v1);
+    public int CalculateNewElo(int userElo, int opponentElo, bool won);
+    public void UpdateTeamElo(TeamModel redTeam, TeamModel blueTeam, bool redTeamWon, bool is1v1);
 }
