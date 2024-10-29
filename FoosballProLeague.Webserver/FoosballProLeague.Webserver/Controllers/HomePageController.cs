f98491ef715da0bf930458add29db084b6484fde
murkch <117634691+xdmurkch@users.noreply.github.com>
2024-10-29 13:01:49 +0100

    Added pagination to the leaderboard, whilst shrinking the vh

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Controllers/HomePageController.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Controllers/HomePageController.cs
@@ -8,39 +8,41 @@ namespace FoosballProLeague.Webserver.Controllers
     public class HomePageController : Controller
     {
         private readonly IHomePageLogic _homePageLogic;
-        private readonly HttpClient _httpClient; // er httpclient nødvendig her? virker overflødig hvis den er korrekt implementeret i HomePageService, no??
 
-        public HomePageController(IHomePageLogic homePageLogic, IHttpClientFactory httpClientFactory)
+        public HomePageController(IHomePageLogic homePageLogic)
         {
             _homePageLogic = homePageLogic;
-            _httpClient = httpClientFactory.CreateClient("APIClient");
         }
 
         [HttpGet("HomePage")]
-        public async Task<IActionResult> HomePage()
+        public async Task<IActionResult> HomePage(int pageNumber = 1, int pageSize = 10)
         {
-            return await GetUsers("1v1");
+            return await GetUsers("1v1", pageNumber, pageSize);
         }
 
         [HttpGet("HomePage/1v1")]
-        public async Task<IActionResult> GetUsers1v1()
+        public async Task<IActionResult> GetUsers1v1(int pageNumber = 1, int pageSize = 10)
         {
-            return await GetUsers("1v1");
+            return await GetUsers("1v1", pageNumber, pageSize);
         }
 
         [HttpGet("HomePage/2v2")]
-        public async Task<IActionResult> GetUsers2v2()
+        public async Task<IActionResult> GetUsers2v2(int pageNumber = 1, int pageSize = 10)
         {
-            return await GetUsers("2v2");
+            return await GetUsers("2v2", pageNumber, pageSize);
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers(string mode)
+        public async Task<IActionResult> GetUsers(string mode, int pageNumber = 1, int pageSize = 10)
         {
             try
             {
-                List<UserModel> users = await _homePageLogic.GetUsers();
+                List<UserModel> users = await _homePageLogic.GetUsers(pageNumber, pageSize);
+                int totalUserCount = await _homePageLogic.GetTotalUserCount();
                 ViewBag.Mode = mode;
+                ViewBag.PageNumber = pageNumber;
+                ViewBag.PageSize = pageSize;
+                ViewBag.TotalUserCount = totalUserCount;
                 return View("HomePage", users);
             }
             catch (Exception ex)
