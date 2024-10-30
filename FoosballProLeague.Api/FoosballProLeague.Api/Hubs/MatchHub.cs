d6a381cd390f3796d463e0e33be2d659b0efc553
nikoUCN <10478737@ucn.dk>
2024-10-30 13:30:03 +0100

    Made a matchStart and MatchEnd method

--- FoosballProLeague.Api/FoosballProLeague.Api/Hubs/MatchHub.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api/Hubs/MatchHub.cs
@@ -5,6 +5,16 @@ namespace FoosballProLeague.Api.Hubs
 {
     public class MatchHub : Hub
     {
+        public async Task NotifyMatchEnd(bool isMatchStart)
+        {
+            await Clients.All.SendAsync("RecieveMatchEnd", isMatchStart);
+        }
+
+        public async Task NotifyMatchStart(bool isMatchStart, TeamModel teamRed, TeamModel teamBlue, int redScore, int blueScore)
+        {
+            await Clients.All.SendAsync("RecieveMatchStart", isMatchStart, teamRed, teamBlue, redScore, blueScore);
+        }
+
         public async Task NotifyGoalsScored(TeamModel teamRed, TeamModel teamBlue, int redScore, int blueScore)
         {
             await Clients.All.SendAsync("RecieveGoalUpdate", teamRed, teamBlue, redScore, blueScore);
