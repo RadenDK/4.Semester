a02700ef81e9d1d695935d967708a15b5218be26
nikoUCN <10478737@ucn.dk>
2024-11-05 13:21:23 +0100

    navbar changes

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/wwwroot/js/layout.js
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/wwwroot/js/layout.js
@@ -1,14 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const profileIcon = document.getElementById('profileIcon');
     const dropdownMenu = document.getElementById('dropdownMenu');
+    const profileIconElement = profileIcon.querySelector('i');
 
-    profileIcon.addEventListener('click', function() {
-        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    profileIcon.addEventListener('click', function (event) {
+        event.stopPropagation();
+        const isVisible = dropdownMenu.classList.contains('show');
+        if (isVisible) {
+            dropdownMenu.classList.remove('show');
+            dropdownMenu.classList.add('hide');
+            setTimeout(() => {
+                dropdownMenu.classList.remove('hide');
+                dropdownMenu.style.display = 'none';
+            }, 300); // Match the duration of the fadeOut animation
+        } else {
+            dropdownMenu.style.display = 'block';
+            dropdownMenu.classList.add('show');
+        }
+        profileIconElement.classList.toggle('active', !isVisible);
     });
 
-    window.addEventListener('click', function(event) {
-        if (!event.target.matches('#profileIcon')) {
-            dropdownMenu.style.display = 'none';
+    document.addEventListener('click', function () {
+        if (dropdownMenu.classList.contains('show')) {
+            dropdownMenu.classList.remove('show');
+            dropdownMenu.classList.add('hide');
+            setTimeout(() => {
+                dropdownMenu.classList.remove('hide');
+                dropdownMenu.style.display = 'none';
+            }, 300); // Match the duration of the fadeOut animation
+            profileIconElement.classList.remove('active');
         }
+    });
+
+    dropdownMenu.addEventListener('click', function (event) {
+        event.stopPropagation();
     });
 });
