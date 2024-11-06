1d85a52961680bd3460b27fd3e3adee72ba77f7e
Rasmus <rasmusjensen.dev@gmail.com>
2024-11-06 11:05:14 +0100

    Extra test for elo gain

--- FoosballProLeague.Api/FoosballProLeague.Api.Tests/ControllerTests/IntergrationTests/MatchControllerTests/EloTests.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api.Tests/ControllerTests/IntergrationTests/MatchControllerTests/EloTests.cs
@@ -425,6 +425,63 @@ namespace FoosballProLeague.Api.Tests.ControllerTests.IntergrationTests.MatchCon
         }
 
         [Fact]
+        public void EloCalculation_MatchTurnsFrom1vs1ToA2vs2_ShouldNotUpdateElo()
+        {
+            // Arrange
+            _dbHelper.InsertData("INSERT INTO users (id, elo_1v1, elo_2v2) VALUES (1, 1000, 1000), (2, 1000, 1000), (3, 1000, 1000), (4, 1000, 1000)");
+            _dbHelper.InsertData("INSERT INTO teams (id, player1_id, player2_id) VALUES (1, 1, null), (2, 2, null)");
+            _dbHelper.InsertData("INSERT INTO teams (id, player1_id, player2_id) VALUES (3, 1, 3), (4, 2, 4)");
+            _dbHelper.InsertData("INSERT INTO foosball_tables (id) VALUES (1)");
+            _dbHelper.InsertData("INSERT INTO foosball_matches (id, table_id, red_team_id, blue_team_id, team_red_score, team_blue_score, ) VALUES (1, 1, 1, 2, 9, 0)");
+            _dbHelper.UpdateData("UPDATE foosball_tables SET active_match_id = 1 WHERE id = 1");
+
+            int mockTableId = 1;
+            RegisterGoalRequest mockRegisterGoalRequestRedSide =
+                new RegisterGoalRequest { TableId = mockTableId, Side = "red" };
+
+            IMatchDatabaseAccessor matchDatabaseAccessor = new MatchDatabaseAccessor(_dbHelper.GetConfiguration());
+            Mock<IHubContext<MatchHub>> mockHubContext = new Mock<IHubContext<MatchHub>>();
+            Mock<IHubClients> mockClients = new Mock<IHubClients>();
+            Mock<IClientProxy> mockClientProxy = new Mock<IClientProxy>();
+
+            mockHubContext.Setup(hub => hub.Clients).Returns(mockClients.Object);
+            mockClients.Setup(clients => clients.All).Returns(mockClientProxy.Object);
+
+            IUserLogic userLogic = new UserLogic(new UserDatabaseAccessor(_dbHelper.GetConfiguration()));
+            IMatchLogic matchLogic = new MatchLogic(matchDatabaseAccessor, (IHubContext<MatchHub>)mockHubContext.Object, userLogic);
+            MatchController SUT = new MatchController(matchLogic);
+
+            // Simulate players joining mid-match
+            TableLoginRequest mockTableLoginRequestPlayer3 = new TableLoginRequest
+
+            { UserId = 3, TableId = mockTableId, Side = "red" };
+            TableLoginRequest mockTableLoginRequestPlayer4 = new TableLoginRequest
+            { UserId = 4, TableId = mockTableId, Side = "blue" };
+
+            SUT.LoginOnTable(mockTableLoginRequestPlayer3);
+            SUT.LoginOnTable(mockTableLoginRequestPlayer4);
+
+
+            // Act
+            IActionResult result = SUT.RegisterGoal(mockRegisterGoalRequestRedSide);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+
+            // Verify ELO updates
+            UserModel user1 = userLogic.GetUserById(1);
+            UserModel user2 = userLogic.GetUserById(2);
+            UserModel user3 = userLogic.GetUserById(3);
+            UserModel user4 = userLogic.GetUserById(4);
+
+            // ELO should not be updated
+            Assert.True(user1.Elo1v1 == 1000 && user1.Elo2v2 == 1000);
+            Assert.True(user2.Elo1v1 == 1000 && user2.Elo2v2 == 1000);
+            Assert.True(user3.Elo1v1 == 1000 && user3.Elo2v2 == 1000);
+            Assert.True(user4.Elo1v1 == 1000 && user4.Elo2v2 == 1000);
+        }
+
+        [Fact]
         public void EloCalculation_EdgeCaseWhereTwoPlayersFromPrevious2v1GameTryToMakeNewTeam_ShouldReturnSucces()
         {
             // Arrange
