a3ddcc5c32df2a39f29cfbd902d75b10ba2e0ec5
Rasmus <rasmusjensen.dev@gmail.com>
2024-11-07 10:11:55 +0100

    I am afraid i just pressed ctrl k ctrl d. I dont think i did anything else but i am afraid not to include it

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Service/LoginService.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Service/LoginService.cs
@@ -10,21 +10,21 @@ namespace FoosballProLeague.Webserver.Service;
 
 public class LoginService : ILoginService
 {
-   private readonly IHttpClientService _httpClientService;
-   
-   public LoginService(IHttpClientService httpClientService)
-   {
-       _httpClientService = httpClientService;
-   }
-   
-   //Service call for login user by calling the HttpClientService
-   // Puts the login data in a object and serializes it
-   public async Task<HttpResponseMessage> LoginUser(string email, string password)
-   {
-       object loginData = new { Email = email, Password = password };
-       StringContent content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");  
-       
-       return await _httpClientService.PostAsync("api/user/login", content);
-   }
+    private readonly IHttpClientService _httpClientService;
+
+    public LoginService(IHttpClientService httpClientService)
+    {
+        _httpClientService = httpClientService;
+    }
+
+    //Service call for login user by calling the HttpClientService
+    // Puts the login data in a object and serializes it
+    public async Task<HttpResponseMessage> LoginUser(string email, string password)
+    {
+        object loginData = new { Email = email, Password = password };
+        StringContent content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
+
+        return await _httpClientService.PostAsync("api/user/login", content);
+    }
 }
 
