744eac8ed9d6e7bb138504675b93a8fd4caebe18
Rasmus <rasmusjensen.dev@gmail.com>
2024-11-22 13:17:13 +0100

    Program.cs update

--- FoosballProLeague.Api/FoosballProLeague.Api/Program.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api/Program.cs
@@ -1,66 +1,90 @@
 using System.Threading.RateLimiting;
+using AspNetCoreRateLimit;
 using FoosballProLeague.Api.BusinessLogic;
 using FoosballProLeague.Api.BusinessLogic.Interfaces;
 using FoosballProLeague.Api.DatabaseAccess;
 using FoosballProLeague.Api.DatabaseAccess.Interfaces;
 using FoosballProLeague.Api.Hubs;
+using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add services to the container.
 builder.Services.AddControllers();
-
-// Register all services as singletons to maintain shared state
-builder.Services.AddSingleton<ICompanyLogic, CompanyLogic>();
-builder.Services.AddSingleton<ICompanyDatabaseAccessor, CompanyDatabaseAccessor>();
-builder.Services.AddSingleton<IUserLogic, UserLogic>();
-builder.Services.AddSingleton<IUserDatabaseAccessor, UserDatabaseAccessor>();
-builder.Services.AddSingleton<IDepartmentLogic, DepartmentLogic>();
-builder.Services.AddSingleton<IDepartmentDatabaseAccessor, DepartmentDatabaseAccessor>();
-builder.Services.AddSingleton<IMatchLogic, MatchLogic>();
-builder.Services.AddSingleton<IMatchDatabaseAccessor, MatchDatabaseAccessor>();
-builder.Services.AddSingleton<ITeamDatabaseAccessor, TeamDatabaseAccessor>();
-builder.Services.AddSingleton<ITokenLogic, TokenLogic>();
-
-// Add SignalR support
 builder.Services.AddSignalR();
 
-// Add CORS with fixed configuration
+// All is added as singleton instead of scoped because matchlogic caches data in forms of pending match teams
+// Because of this, we want to make sure that the same instance is used for all requests
+// If it were scoped, it would be created for each request, and the cache would be empty for each request
+builder.Services.AddSingleton<ICompanyLogic, CompanyLogic>();
+builder.Services.AddSingleton<ICompanyDatabaseAccessor, CompanyDatabaseAccessor>();
+
+builder.Services.AddSingleton<IUserLogic, UserLogic>();
+builder.Services.AddSingleton<IUserDatabaseAccessor, UserDatabaseAccessor>();
+
+builder.Services.AddSingleton<IDepartmentLogic, DepartmentLogic>();
+builder.Services.AddSingleton<IDepartmentDatabaseAccessor, DepartmentDatabaseAccessor>();
+
+builder.Services.AddSingleton<IMatchLogic, MatchLogic>();
+builder.Services.AddSingleton<IMatchDatabaseAccessor, MatchDatabaseAccessor>();
+
+builder.Services.AddSingleton<ITeamDatabaseAccessor, TeamDatabaseAccessor>();
+
+builder.Services.AddSingleton<ITokenLogic, TokenLogic>();
+
+// Add Rate limiting services
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("RatePolicy", confic =>
+    
+    {
+        confic.Window = TimeSpan.FromMinutes(1);
+        confic.PermitLimit = 100;
+        confic.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        confic.QueueLimit = 1;
+    });
+});
+
+//cors
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
     {
-        builder.SetIsOriginAllowed(_ => true) // More secure than AllowAnyOrigin
-               .AllowAnyMethod()
+        builder.AllowAnyMethod()
                .AllowAnyHeader()
+               .SetIsOriginAllowed(origin => true) // Allows all origins
                .AllowCredentials();
     });
 });
 
-// Add Swagger for API documentation
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// IMPORTANT: UseCors must be called before UseRouting and MapHub
 app.UseCors("CorsPolicy");
 
-app.UseRouting(); // Add this line
+//app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-// Map SignalR hub
-app.MapHub<HomepageHub>("/homepageHub"); // Make sure to specify the Hub class
+// Use rate limiting middleware
+app.UseRateLimiter();
+app.MapControllers().RequireRateLimiting("RatePolicy");
 
-// Map controllers
 app.MapControllers();
+
+// Map the SignalR hub
+app.MapHub<HomepageHub>("/homepageHub");
+
 
 app.Run();
