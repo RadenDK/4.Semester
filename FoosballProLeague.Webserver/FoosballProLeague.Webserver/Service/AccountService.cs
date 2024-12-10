fad3abfdc1bedc421cfdb3111602ae04364afc03
nikoUCN <10478737@ucn.dk>
2024-12-10 13:34:30 +0100

    service for account

---
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Service/AccountService.cs
@@ -0,0 +1,38 @@
+﻿using FoosballProLeague.Webserver.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace FoosballProLeague.Webserver.Service
+{
+    public class AccountService : IAccountService
+    {
+
+        private readonly IHttpClientService _httpClientService;
+
+        public AccountService(IHttpClientService httpClientService)
+        {
+            _httpClientService = httpClientService;
+        }
+
+        public async Task<HttpResponseMessage> FindUserByEmail(string email)
+        {
+            return await _httpClientService.GetAsync("api/User/user");
+        }
+
+        public async Task<HttpResponseMessage> ResetPassword(string email, string password)
+        {
+            object updateData = new { Email = email, Password = password };
+            StringContent content = new StringContent(JsonConvert.SerializeObject(updateData), Encoding.UTF8, "application/json");
+
+            return await _httpClientService.PutAsync("api/User/reset-password", content);
+        }
+
+        public async Task<HttpResponseMessage> SendEmail(string email)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(email), Encoding.UTF8, "application/json");
+
+            return await _httpClientService.PostAsync("api/User/send-email", content);
+        }
+    }
+}
---
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Service/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿namespace FoosballProLeague.Webserver.Service
+{
+    public interface IAccountService
+    {
+        public Task<HttpResponseMessage> FindUserByEmail(string email);
+        public Task<HttpResponseMessage> ResetPassword(string email, string password);
+        public Task<HttpResponseMessage> SendEmail(string email);
+    }
+}
