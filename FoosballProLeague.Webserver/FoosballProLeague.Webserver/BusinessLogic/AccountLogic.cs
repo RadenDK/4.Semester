0db6731bc07d725d73bb87c06b2f1b79442a3840
nikoUCN <10478737@ucn.dk>
2024-12-10 13:34:39 +0100

    logic for account

---
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/BusinessLogic/AccountLogic.cs
@@ -0,0 +1,32 @@
+﻿using FoosballProLeague.Webserver.Models;
+using FoosballProLeague.Webserver.Service;
+
+namespace FoosballProLeague.Webserver.BusinessLogic
+{
+    public class AccountLogic : IAccountLogic
+    {
+
+        private readonly IAccountService _accountService;
+
+
+        public AccountLogic(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public async Task<HttpResponseMessage> FindUserByEmail(string email)
+        {
+            return await _accountService.FindUserByEmail(email);
+        }
+
+        public async Task SendEmail(string email)
+        {
+            await _accountService.SendEmail(email);
+        }
+
+        public async Task<HttpResponseMessage> ResetPassword(string email, string newPassword)
+        {
+            return await _accountService.ResetPassword(email, newPassword);
+        }
+    }
+}
---
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/BusinessLogic/IAccountLogic.cs
@@ -0,0 +1,11 @@
+﻿using FoosballProLeague.Webserver.Models;
+
+namespace FoosballProLeague.Webserver.BusinessLogic
+{
+    public interface IAccountLogic
+    {
+        public Task<HttpResponseMessage> FindUserByEmail(string email);
+        public Task SendEmail(string email);
+        public Task<HttpResponseMessage> ResetPassword(string email, string newPassword);
+    }
+}
