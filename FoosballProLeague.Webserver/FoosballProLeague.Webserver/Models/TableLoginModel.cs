8f6f37df49c9098644b34ef1653710cd30432ad5
nikoUCN <10478737@ucn.dk>
2024-12-11 13:38:16 +0100

    UI for qrkode

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/TableLoginModel.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/TableLoginModel.cs
@@ -4,5 +4,7 @@ public class TableLoginModel
 {
     public int TableId { get; set; }
     public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Side { get; set; }
 }
