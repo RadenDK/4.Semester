68fc461a635d95a4a9d9f2e601cceb96d13114e0
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-12-13 11:55:40 +0100

    deleted name from model

--- FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/TableLoginModel.cs
+++ FoosballProLeague.Webserver/FoosballProLeague.Webserver/Models/TableLoginModel.cs
@@ -4,7 +4,5 @@ public class TableLoginModel
 {
     public int TableId { get; set; }
     public string Email { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
     public string Side { get; set; }
 }
