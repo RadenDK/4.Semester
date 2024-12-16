f241dc01dc801de8c4183cda206b848e8eff9ae1
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-12-16 17:31:08 +0100

    added name

--- FoosballProLeague.Api/FoosballProLeague.Api/Models/RequestModels/TableLoginRequest.cs
+++ FoosballProLeague.Api/FoosballProLeague.Api/Models/RequestModels/TableLoginRequest.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string Email { get; set; }
         public int TableId { get; set; }
         public string Side { get; set; }
